6cf063c5c7ebff17d4078182b50c06a2af1f0cc0
Marcos Gutiérrez <marcosgtzx7@hotmail.com>
2025-05-26 12:13:25 -0600

    Se hicieron cambios

---
+++ Ejercicio2.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Ejercicio2 {
+ public static void main(String[] args) {
+        
+        int[] numeros = new int[12];
+
+       
+        Scanner sc = new Scanner(System.in);
+
+        
+        int suma = 0;
+
+        
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.print("Ingresa el número #" + (i + 1) + ": ");
+            numeros[i] = sc.nextInt();       
+            suma += numeros[i];             
+        }
+
+        
+        double promedio = (double) suma / numeros.length;
+
+        
+        System.out.println("\nEl promedio de los números ingresados es: " + promedio);
+
+    }
+}
